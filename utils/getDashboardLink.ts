066d38dc58d86e6841319a5c10b24c399f2eb96f
kryptocodes <srivatsantb@gmail.com>
2024-10-10 12:47:03 +0530

    fix: notification loop
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- utils/getDashboardLink.ts
+++ utils/getDashboardLink.ts
@@ -1,3 +1,4 @@
+// todo: replace this with db query 
 const grants = [
     {
       name: 'starknet',
@@ -7,6 +8,10 @@ const grants = [
         name: 'ens',
         grants: ['6619151a3a7a91313846ed80']
     },
+    {
+       name: 'axelar',
+       grants: ['661e3ce1f056dd981db4e795','661e3cc3f056dd981db4e6a6','65fad2b01080cbb344dbbf24','661e3c82f056dd981db4e293','661e3bf5ccf6446509d2b308','669778c4e1827e9ac9693bf5','661e3ca0f056dd981db4e4a5']
+    }
 ];
 
 const getDashboardLink = (grant: string, chain: string, appId?: string): string => {
--- utils/sendNotifications.ts
+++ utils/sendNotifications.ts
@@ -1,8 +1,6 @@
 import {
   DynamoDBClient,
   GetItemCommand,
-  PutItemCommand,
-  UpdateItemCommand,
 } from "@aws-sdk/client-dynamodb";
 import { unmarshall } from "@aws-sdk/util-dynamodb";
 import { APIGatewayProxyEvent, Context } from "aws-lambda";
@@ -19,163 +17,128 @@ import { getMessage } from "./getMessage";
 import { sleep } from "./sleep";
 
 export const run = async (event: APIGatewayProxyEvent, context: Context) => {
+  try{
   // 1. Get the current timestamp
   const date = new Date();
   const currentTimestamp = Math.floor(date.getTime() / 1000);
 
-  // 2. Fetch the last timestamp when this function was executed
-  const client = new DynamoDBClient({ region: "ap-south-1" });
-  const command = new GetItemCommand({
-    TableName: process.env.TABLE,
-    Key: { key: { S: "last-fetched" } },
-  });
-  const response = await client.send(command);
-  if (!response?.Item?.timestamp) {
-    const updateItemCommand = new PutItemCommand({
-      TableName: process.env.TABLE,
-      Item: {
-        key: { S: "last-fetched" },
-        timestamp: { N: currentTimestamp.toString() },
-      },
-    });
+ // 2. Last fetched timestamp 
+ const client = new DynamoDBClient({ region: "ap-south-1" });
+  const lastFetchedTimestamp = currentTimestamp - 60 * 5; // 5 minutes ago
+  console.log("lastFetchedTimestamp: ", lastFetchedTimestamp, currentTimestamp);
 
-    const updateResponse = await client.send(updateItemCommand);
-    if (updateResponse?.$metadata?.httpStatusCode !== 200) {
-      console.log("Could not set current time!");
+  // 3. Fetch the notifications from the subgraph
+  const chain = 10;
+  const first = 100;
+  const skip = 0;
+
+  const graphQLClient = new GraphQLClient(subgraphURLS[chain]);
+  const notifs: GetNotificationsQuery["notifications"] = [];
+
+  const res: GetNotificationsQuery = await graphQLClient.request(
+    GetNotifications,
+    {
+      first,
+      skip,
+      from: lastFetchedTimestamp,
+      to: currentTimestamp,
     }
+  );
+
+  if (!res.notifications || res.notifications.length === 0) {
+    console.log("No new notifications found on chain id: ", chain);
     return;
   }
-  const lastFetchedTimestamp = parseInt(response?.Item?.timestamp.N);
-  console.log("lastFetchedTimestamp: ", lastFetchedTimestamp, currentTimestamp);
-  // 3. For each chain, fetch the notifications from the subgraph, and send out Telegram messages
-  for (const chain in subgraphURLS) {
-    const first = 100;
-    let skip = 0;
-    const MAX_ITERATIONS = 10;
-    let iterations = 0;
 
+  notifs.push(...res.notifications);
 
-    const graphQLClient = new GraphQLClient(subgraphURLS[chain]);
-    const notifs: GetNotificationsQuery["notifications"] = [];
-    while (iterations < MAX_ITERATIONS) {
-      const res: GetNotificationsQuery = await graphQLClient.request(
-        GetNotifications,
-        {
-          first,
-          skip,
-          from: lastFetchedTimestamp,
-          to: currentTimestamp,
-        }
-      );
+  const bot = new Telegraf(process.env.BOT_TOKEN);
+  const receivedUsers: string[] = [];
 
-      notifs.push(...res.notifications);
+  for (const notif of notifs) {
+    if (permittedNotificationTypes.indexOf(notif.type) === -1) continue;
 
-      if (res.notifications.length < first) break;
-      skip += first;
-      iterations++;
-    }
-
-    if (notifs.length === 0) {
-      console.log("No new notifications found on chain id: ", chain);
-      continue;
-    }
-
-    const bot = new Telegraf(process.env.BOT_TOKEN);
-    for (const notif of notifs) {
-      if (permittedNotificationTypes.indexOf(notif.type) === -1) continue;
-
-      const entityInfo: GetEntityQuery = await graphQLClient.request(
-        GetEntity,
-        {
-          grantId:
-            notif.entityIds.find((e) => e.startsWith("grant"))?.split("-")[1] ??
-            "0x0000000000000000000000000000000000000000",
-          appId:
-            notif.entityIds
-              .find((e) => e.startsWith("application"))
-              ?.split("-")[1] ?? "0x0",
-          timestamp: notif.cursor,
-        }
-      );
-      const receivedUsers: string[] = []
-      for (const entity of notif.entityIds) {
-        // 1. Fetch the list of users subscribed to this entity
-        let type = entity.startsWith("grant")
-          ? "gp"
-          : entity.startsWith("application")
-            ? "app"
-            : "";
-        if (type === "") continue;
-
-        const key = `${type}-${entity.split("-")[1]}-${chain}`;
-        const command = new GetItemCommand({
-          TableName: process.env.TABLE,
-          Key: { key: { S: key } },
-        });
-
-        const response = await client.send(command);
-        if (response?.$metadata?.httpStatusCode !== 200) {
-          console.log(
-            `Could not fetch subscribed IDs for entity: ${entity} (${notif.id})`
-          );
-        }
-
-        if (!response?.Item) continue;
-
-        const users = unmarshall(response?.Item);
-        delete users.key;
-        // console.log(users);
-
-        // 2. Send a notification to each user - based on what type of notification it is
-        let count = 0;
-
-        const message = await getMessage(
-          type as "gp" | "app",
-          chain.toString(),
-          entityInfo,
-          notif
-        );
-
-        if (message === "") continue;
-
-        for (const user in users) {
-          if (receivedUsers.find(element => element==user)) continue
-          receivedUsers.push(user)
-
-          console.log(user, users[user]);
-          
-          const chatId = users[user];
-          try {
-            await bot.telegram.sendMessage(chatId, message, {
-              parse_mode: "HTML",
-            });
-            ++count;
-            if (count > 20) {
-              // Wait for 1 second if the list of users > 30 - since Telegram only permits 30 messages per second
-              count = 0;
-              await sleep(1000);
-            }
-          } catch (e) {
-            console.error("Error sending message to chatId: ", chatId, e);
-          }
-        }
-
-        await sleep(1000);
+    const entityInfo: GetEntityQuery = await graphQLClient.request(
+      GetEntity,
+      {
+        grantId:
+          notif.entityIds.find((e) => e.startsWith("grant"))?.split("-")[1] ??
+          "0x0000000000000000000000000000000000000000",
+        appId:
+          notif.entityIds
+            .find((e) => e.startsWith("application"))
+            ?.split("-")[1] ?? "0x0",
+        timestamp: notif.cursor,
       }
+    );
+
+    for (const entity of notif.entityIds) {
+      // 1. Fetch the list of users subscribed to this entity
+      let type = entity.startsWith("grant")
+        ? "gp"
+        : entity.startsWith("application")
+          ? "app"
+          : "";
+      if (type === "") continue;
+
+      const key = `${type}-${entity.split("-")[1]}-${chain}`;
+      const command = new GetItemCommand({
+        TableName: process.env.TABLE,
+        Key: { key: { S: key } },
+      });
+
+      const response = await client.send(command);
+      if (response?.$metadata?.httpStatusCode !== 200) {
+        console.log(
+          `Could not fetch subscribed IDs for entity: ${entity} (${notif.id})`
+        );
+      }
+
+      if (!response?.Item) continue;
+
+      const users = unmarshall(response?.Item);
+      delete users.key;
+
+      // 2. Send a notification to each user - based on what type of notification it is
+      let count = 0;
+
+      const message = await getMessage(
+        type as "gp" | "app",
+        chain.toString(),
+        entityInfo,
+        notif
+      );
+
+      if (message === "") continue;
+
+      for (const user in users) {
+        if (receivedUsers.find(element => element==user)) continue;
+        receivedUsers.push(user);
+
+        console.log(user, users[user]);
+
+        const chatId = users[user];
+        try {
+          await bot.telegram.sendMessage(chatId, message, {
+            parse_mode: "HTML",
+          });
+          ++count;
+          if (count > 20) {
+            // Wait for 1 second if the list of users > 20 - since Telegram only permits 30 messages per second
+            count = 0;
+            await sleep(1000);
+          }
+        } catch (e) {
+          console.error("Error sending message to chatId: ", chatId, e);
+        }
+      }
+
+      await sleep(1000);
     }
   }
 
-  // 4. Update the timestamp in the database
-  const updateItemCommand = new PutItemCommand({
-    TableName: process.env.TABLE,
-    Item: {
-      key: { S: "last-fetched" },
-      timestamp: { N: currentTimestamp.toString() },
-    },
-  });
-
-  const updateResponse = await client.send(updateItemCommand);
-  if (updateResponse?.$metadata?.httpStatusCode !== 200) {
-    console.log("Could not set current time!");
+  console.log("Finished sending notifications");
+  } catch (e) {
+    console.log(`Error: ${e}`);
   }
 };
